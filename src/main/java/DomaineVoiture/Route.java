fab5ee06a97aee3ee0d11c62790198338f8e8c5b
flogar <flo.garnier31@gmail.com>
2016-03-15 11:07:57 +0100

    commit route design

---
+++ src/main/java/DomaineVoiture/Route.java
@@ -0,0 +1,20 @@
+package DomaineVoiture;
+
+/**
+ * Created by flori on 15/03/2016.
+ */
+public class Route {
+    private int debx;
+    private int deby;
+    private int finx;
+    private int finy;
+
+
+    public Route(int x, int y, int x2, int y2) {
+        debx = x;
+        deby = y;
+        finx = x2;
+        finy = y2;
+    }
+
+}
